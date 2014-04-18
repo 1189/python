1f4d362fb38e6a965eeb97a9d3249282b3cd3736
1189 <1189@gmx.com>
2014-04-18 21:15:32 +0800

    Create ex12.py

---
+++ ex12.py
@@ -0,0 +1,13 @@
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weigh? ")
+
+print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
+    
+    
+    
+    
+brothers = raw_input("How many brothers do you have? ")
+sisters = raw_input("How many sisters do you have? ")
+print "So you have %s brothers and %s sisters." % (brothers, sisters)
