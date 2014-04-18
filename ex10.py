b68232ab9b65d5df8e338eadf33d33636e4a6774
1189 <1189@gmx.com>
2014-04-18 21:14:08 +0800

    Create ex10.py

---
+++ ex10.py
@@ -0,0 +1,26 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
+
+
+
+a = "\tWhat's my name?"
+b = "My name is...\nEric."
+c = "I'm 6'8\" tall."
+
+print a
+print b
+print c
